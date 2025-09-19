1878f184146564ef0190c12f19f536333e840c2d
traoreab <traorebasile98@gmail.com>
2025-09-20 00:03:32 +0100

    fix: type check

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,6 +1,6 @@
 import { connect, set } from 'mongoose';
 
-const MONGO_DB_URI = process.env.MONGO_DB_URI;
+const MONGO_DB_URI = process.env.MONGO_DB_URI ?? "";
 
 // connection to db
 export const connectToDB = async () => {
