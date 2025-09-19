b3903f26ba41fd48becd48c7e28aa832f602bbba
traoreab <traorebasile98@gmail.com>
2025-09-19 23:51:51 +0100

    feat: add db

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from "./app"
 import { config } from "./config"
+import { connectToDB } from "./config/mongoose"
 
+connectToDB()
 app.listen(config.port, () => {
   console.log(
     `${config.nodeEnv} - Server is running on http://localhost:${config.port}`
