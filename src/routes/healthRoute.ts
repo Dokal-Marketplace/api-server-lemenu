8e87d0ddd032658b8cb7dc959cb3cc3afab155d8
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2025-09-21 14:16:19 +0000

    disable security for healthcheck route (#7)
    * feat: add healthcheck
    
    * feat: disable healthcheck security
    
    * add db models (#6)
    
    * fix pnpm conflicts (#5)
    
    * feat: add db
    
    * feat: add missing body-parser
    
    * fix: remove body-parser
    
    * Update pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: traoreab <traorebasile98@gmail.com>
    
    * fix: deps conflicts
    
    * feat: add models
    
    * feat: update types path
    
    * fix: build errors
    
    ---------
    
    Co-authored-by: traoreab <traorebasile98@gmail.com>
    
    ---------
    
    Co-authored-by: traoreab <traorebasile98@gmail.com>

--- src/routes/healthRoute.ts
+++ src/routes/healthRoute.ts
@@ -1,9 +1,8 @@
 import { Router } from "express"
 import { getHealth } from "../controllers/healthController"
-import { tokenAuthHandler } from "../middleware/tokenAuthHandler"
 
 const router = Router()
 
-router.get("/", tokenAuthHandler, getHealth)
+router.get("/", getHealth)
 
 export default router
