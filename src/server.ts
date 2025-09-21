60497a9a7956256944bf70fc66e18c36db0e3ccd
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2025-09-21 17:40:56 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app from "./app"
 import { config } from "./config"
+import { connectToDB } from "./config/mongoose";
 
 const version = `v1`;
 const baseRoute = `api`;
-
+connectToDB()
 app.listen(config.port, () => {
   console.log(
     `${config.nodeEnv} - Server is running on http://localhost:${config.port}/${baseRoute}/${version}`
