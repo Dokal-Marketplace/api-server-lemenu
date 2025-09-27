4d2efdf6a04a591ad376ca9ac0c53a07e187eae6
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2025-09-27 15:47:52 +0000

    feat: update roles assignements (#33)
    * feat: add user role
    
    * fix: user registration
    
    * fix: user registration
    
    ---------
    
    Co-authored-by: traoreab <traorebasile98@gmail.com>

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true },
+    role: { type: String, required: true },
     firstName: { type: String, required: true, trim: true },
     lastName: { type: String, required: true, trim: true },
     restaurantName: { type: String, default: null },
